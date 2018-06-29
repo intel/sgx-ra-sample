ea82411136768c658f57a222d87ad2a4b2c714fa
John Mechalas <johnm@sgxvmubuntu01.jf.intel.com>
2018-06-29 11:30:48 -0700

    Fix/add license headers. Remove ^M's

--- base64.h
+++ base64.h
@@ -1,3 +1,20 @@
+/*
+
+Copyright 2018 Intel Corporation
+
+This software and the related documents are Intel copyrighted materials,
+and your use of them is governed by the express license under which they
+were provided to you (License). Unless the License provides otherwise,
+you may not use, modify, copy, publish, distribute, disclose or transmit
+this software or the related documents without Intel's prior written
+permission.
+
+This software and the related documents are provided as is, with no
+express or implied warranties, other than those that are expressly stated
+in the License.
+
+*/
+
 #ifndef __BASE64_H
 #define __BASE64_H
 
